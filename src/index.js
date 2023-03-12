6bf92df6354a054aedb0085bdc017c8f57913d25
insomniac2305 <84805847+insomniac2305@users.noreply.github.com>
2023-03-12 15:36:06 +0100

    Add board listeners

--- src/index.js
+++ src/index.js
@@ -1,2 +1,24 @@
+import game from "./game";
 import "./styles.css";
-console.log("hello world!");
+
+const newGame = game();
+newGame.init();
+
+function fireShot(col ,row) {
+  newGame.takeTurn(col, row);
+  const gameboard = document.querySelector(".gameboard.inactive");
+  // eslint-disable-next-line no-use-before-define
+  addBoardListeners(gameboard);
+}
+
+function addBoardListeners(gameboard) {
+  const columns = gameboard.querySelectorAll(".col[data-col]");
+  columns.forEach((col) => {
+    const rows = col.querySelectorAll(".row[data-row]:not(.hit, .missed)");
+    rows.forEach((row) => {
+      row.addEventListener("click", fireShot.bind(this, col.dataset.col, row.dataset.row));
+    });
+  });
+}
+
+addBoardListeners(document.querySelector(".gameboard.inactive"));
